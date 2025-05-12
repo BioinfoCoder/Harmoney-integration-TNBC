3a248ac6f6285b565e82cab37b2f270ce1e41ab7
Doaa Hussein Hassan Dewedar <mri.doaa.m.informatics19@alexu.edu.eg>
2025-05-12 22:54:29 +0300

    Update Harmoney with pathological annotation.R

--- Harmoney with pathological annotation.R
+++ Harmoney with pathological annotation.R
@@ -2,13 +2,18 @@ library(harmony)
 library(Seurat)
 library(dplyr)
 library(cowplot)
-seu <- NormalizeData(seu)
-seu <- FindVariableFeatures(seu)
-seu <- ScaleData(seu)
-seu <- RunPCA(seu, npcs = 30)  # adjust number of PCs as needed
+library(dplyr)
+
+combined_seura@meta.data$Classification[combined_seura@meta.data$Classification == ""] <- "Unknown"
+
+unique(combined_seura@meta.data$Classification)
+se <- NormalizeData(combined_seura)
+se <- FindVariableFeatures(se)
+se <- ScaleData(se)
+se <- RunPCA(se, npcs = 30)  # adjust number of PCs as needed
 
 ## run harmony with default parameters
-pbmc <- seu %>% RunHarmony("Classification")
+pbmc <- se %>% RunHarmony("Classification")
 pbmc <- pbmc %>% 
   RunHarmony("Classification", plot_convergence = TRUE, nclust = 50, max_iter = 10, early_stop = T)
 
@@ -37,6 +42,4 @@ pbmc <- pbmc %>%
 p1 <- DimPlot(pbmc, reduction = "umap", group.by = "Classification", pt.size = .1)
 p2 <- DimPlot(pbmc, reduction = "umap", label = TRUE,  pt.size = .1)
 plot_grid(p1, p2)
-sessionInfo()
-FeaturePlot(object = pbmc, features= c("CD3D", "SELL", "CREM", "CD8A", "GNLY", "CD79A", "FCGR3A", "CCL2", "PPBP"), 
-            min.cutoff = "q9", cols = c("lightgrey", "blue"), pt.size = 0.5)
+
